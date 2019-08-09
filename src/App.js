b6b4373a0f1666862444b739ee9abbe05655f7d0
Alexander <alexander@Alexanders-MacBook-Pro.local>
2019-08-09 11:36:21 +0200

    Added router and some setup and cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styling/main.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Pages
+import Start from './pages/start'
+import NotFound from './pages/404'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Route exact path='/' component={Start} />
+
+			{/* Keep this last as a default route */}
+			<Route path='*' component={NotFound} />
+		</Router>
+	);
 }
 
 export default App;
